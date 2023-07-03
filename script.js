76144badca2afa4962b6dc6cd1246e051396df9a
Jack Sebben <jacksebben11@hotmail.com>
2023-07-03 18:50:01 -0400

    Added my name and tagline along with some CSS animations for the name to slide in from each side. Also, added a fade-in animation to the tagline so that it can fade in immediately after the first and last name slide in.

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const firstName = document.querySelector('.first-name');
-    const lastName = document.querySelector('.last-name');
-    const tagLine = document.querySelector('.tagline');
-  
-    // Wait for slide-in animations to finish before starting color transition and fadeIn animations
-    setTimeout(() => {
-      firstName.style.animation = 'colorTransition 10s ease-in-out infinite';
-      lastName.style.animation = 'colorTransition 10.1s ease-in-out infinite';
-  
-      // Fade-in tagline
-      tagLine.style.opacity = '1';
-    }, 1000); // Wait 1000ms (1 second) before starting color transition and fadeIn animations
-  });
+//    SELECTORS   //
+
+const firstName = document.querySelector('.first-name');
+const lastName = document.querySelector('.last-name');
+const tagLine = document.querySelector('.tagline');
   
